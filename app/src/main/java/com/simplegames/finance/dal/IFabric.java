e8a71287af90749ae54a03a79ca88631dfd24e6b
AndryKakin <andrewsw@mail.ru>
2014-10-11 17:29:08 +0400

    Work by shops.

--- app/src/main/java/com/simplegames/finance/dal/IFabric.java
+++ app/src/main/java/com/simplegames/finance/dal/IFabric.java
@@ -1,10 +1,12 @@
 package com.simplegames.finance.dal;
 
 import com.simplegames.finance.models.Product;
+import com.simplegames.finance.models.Shop;
 
 /**
  * Created by andrey.kakin on 03.10.14.
  */
 public interface IFabric {
     public IRepository<Product> GetProductRepository();
+    public IRepository<Shop> GetShopRepository();
 }
--- app/src/main/java/com/simplegames/finance/dal/SQLiteDbFabric.java
+++ app/src/main/java/com/simplegames/finance/dal/SQLiteDbFabric.java
@@ -3,7 +3,9 @@ package com.simplegames.finance.dal;
 import android.content.Context;
 
 import com.simplegames.finance.dal.product.ProductRepository;
+import com.simplegames.finance.dal.shop.ShopRepository;
 import com.simplegames.finance.models.Product;
+import com.simplegames.finance.models.Shop;
 
 /**
  * Created by andrey.kakin on 03.10.14.
@@ -14,5 +16,10 @@ public class SQLiteDbFabric implements IFabric{
     @Override
     public IRepository<Product> GetProductRepository() {
         return new ProductRepository(Context);
+    }
+
+    @Override
+    public IRepository<Shop> GetShopRepository() {
+        return new ShopRepository(Context);
     }
 }
