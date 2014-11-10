aac082dbccf722146dc65ca5f27c7185b2ec62a4
Ailend <andrewsw@mail.ru>
2014-11-10 20:03:44 +0300

    test

---
+++ app/src/main/java/com/simplegames/finance/ViewModels/OperationItemsLayout.java
@@ -0,0 +1,15 @@
+package com.simplegames.finance.ViewModels;
+
+/**
+ * Created by andrey.kakin on 10.11.2014.
+ */
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class OperationItemsLayout extends LinearLayout {
+
+    public OperationItemsLayout(Context context) {
+        super(context);
+    }
+}
