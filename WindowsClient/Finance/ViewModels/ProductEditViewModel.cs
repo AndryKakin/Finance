1e4e7340349ebfbe0303464370cb3dc5912ca672
Andrey Kakin <andrey.kakin@jeppesen.com>
2015-12-16 19:37:10 +0300

    Add product manager and viewmodel.

---
+++ WindowsClient/Finance/ViewModels/ProductEditViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Models.Products;
+
+namespace Finance.ViewModels
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class ProductEditViewModel
+    {
+        private ProductManager _productManager;
+
+        [ImportingConstructor]
+        public ProductEditViewModel(ProductManager productManager)
+        {
+            _productManager = productManager;
+            LoadProductCommand = new DelegateCommand(LoadAllProducts);
+        }
+
+        private void LoadAllProducts()
+        {
+            
+        }
+
+        public ICommand LoadProductCommand { get; set; }
+    }
+}
--- WindowsClient/Models/Products/ProductManager.cs
+++ WindowsClient/Models/Products/ProductManager.cs
@@ -1,10 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
 using Google.Protobuf;
 using Google.Protobuf.Products;
 using Grpc.Core;
+using Status = Google.Protobuf.Products.Status;
 
 namespace Models.Products
 {
+    [Export]
     public class ProductManager
     {
         private string _addresses;
@@ -18,17 +26,50 @@ namespace Models.Products
         public void Add(Product productModel)
         {
             Channel channel = new Channel(_addresses, Credentials.Insecure);
-
+            
             var client = ProductService.NewClient(channel);
             var request = new Google.Protobuf.Products.Product
             {
                 Name = productModel.Name,
                 Description = productModel.Description,
-                //Bitmap = productModel.Bitmap.Save()
-            };
-
+                Bitmap = GetValue(productModel.Bitmap)
+        };
             request.Bitmap = ByteString.CopyFrom(File.ReadAllBytes("Lighthouse.jpg"));
-            var resuts = client.GetAll(new GetAllRequest());
+            var result = client.AddProduct(request);
+            if(result.Status == Status.Failed)
+                throw new Exception();
+        }
+
+        private static ByteString GetValue(Bitmap bitmap)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, ImageFormat.Jpeg);
+                memoryStream.Position = 0;
+                return ByteString.CopyFrom(memoryStream.ToArray());
+            }
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            Channel channel = new Channel(_addresses, Credentials.Insecure);
+
+            var client = ProductService.NewClient(channel);
+            var results = client.GetAll(new GetAllRequest());
+            return results.Products.Select(dtoProduct =>
+            {
+                using (var memoryStream= new MemoryStream(dtoProduct.Bitmap.ToByteArray()))
+                {
+                    memoryStream.Position = 0;
+                    return new Product
+                    {
+                        Id = dtoProduct.Id,
+                        Name = dtoProduct.Name,
+                        Description = dtoProduct.Description,
+                        Bitmap = new Bitmap(memoryStream)
+                    };
+                }
+            });
         }
     }
 }
